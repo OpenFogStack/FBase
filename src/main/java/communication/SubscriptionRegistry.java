866bf4abfd1dd2a64c9e3b708a14c5fb5e85b8df
dbermbach <dbermbach@gmail.com>
2017-07-27 10:28:38 +0200

    light refactoring

--- src/main/java/communication/SubscriptionRegistry.java
+++ src/main/java/communication/SubscriptionRegistry.java
@@ -1,25 +1,28 @@
 package communication;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import model.data.KeygroupID;
 
 import org.apache.log4j.Logger;
 
 import control.FBase;
 import crypto.CryptoProvider.EncryptionAlgorithm;
-import model.data.KeygroupID;
 
 public class SubscriptionRegistry {
 
-	private static Logger logger = Logger.getLogger(SubscriptionRegistry.class.getName());
+	private static Logger logger = Logger.getLogger(SubscriptionRegistry.class
+			.getName());
 
-	private volatile HashMap<String, HashMap<Integer, Subscriber>> activeSubscriptions = null;
+	private final Map<String, Map<Integer, Subscriber>> activeSubscriptions = new HashMap<>();
 	private FBase fBase;
 
 	public SubscriptionRegistry(FBase fBase) {
 		this.fBase = fBase;
-		activeSubscriptions = new HashMap<String, HashMap<Integer, Subscriber>>();
 	}
-	
+
 	/**
 	 * Starts a new Subscriber.
 	 * 
@@ -31,38 +34,53 @@ public class SubscriptionRegistry {
 	 *            - the secret used to decrypt received data
 	 * @param algorithm
 	 *            - the algorithm used for decryption
-	 * @return the subscriber or null
+	 * @return the new subscriber or null
 	 */
-	public synchronized Subscriber subscribeTo(String address, int port, String secret,
-			EncryptionAlgorithm algorithm, KeygroupID keygroupIDFilter) {
+	public synchronized Subscriber subscribeTo(String address, int port,
+			String secret, EncryptionAlgorithm algorithm,
+			KeygroupID keygroupIDFilter) {
 		// Case 1: address port combination exists -> false
 		if (subscriptionExists(address, port)) {
 			logger.warn("Already subscribed to " + address + ":" + port);
 			return null;
 		}
-
+		// Case 2: subscription does not exist yet
+		// create subscriber
 		Subscriber subscriber = null;
 		try {
-			subscriber = new Subscriber(address, port, secret, algorithm, fBase, keygroupIDFilter);
+			subscriber = new Subscriber(address, port, secret, algorithm,
+					fBase, keygroupIDFilter);
 			if (subscriber.startReception() == null) {
-				throw new Exception("Could not start receiving.");
+				throw new RuntimeException("Could not start receiving.");
 			}
 		} catch (Exception e) {
 			logger.error("Could not initialize Subscriber");
 			e.printStackTrace();
 		}
-		
-		// Case 2: subscriptions for address exist -> put port in already existent hashmap
-		if (subscribedToAddress(address)) {
-			activeSubscriptions.get(address).put(port, subscriber);
-			return subscriber;
+		// add subscriber to map
+		Map<Integer, Subscriber> map = getOrCreateInnerMap(address);
+
+		map.put(port, subscriber);
+
+		return subscriber;
+	}
+
+	/**
+	 * gets or create the inner map associated with the specified address
+	 * 
+	 * 
+	 * @param address
+	 * @return the inner map associated with the specified address
+	 */
+	private synchronized Map<Integer, Subscriber> getOrCreateInnerMap(
+			String address) {
+		Map<Integer, Subscriber> tmp = activeSubscriptions.get(address);
+		if (tmp == null) {
+			tmp = new ConcurrentHashMap<>();
+			activeSubscriptions.put(address, tmp);
 		}
 
-		// Case 3: not subscribed to address yet -> create new hashmap
-		HashMap<Integer, Subscriber> map = new HashMap<Integer, Subscriber>();
-		map.put(port, subscriber);
-		activeSubscriptions.put(address, map);
-		return subscriber;
+		return tmp;
 	}
 
 	public synchronized boolean unsubscribeFrom(String address, int port) {
@@ -71,23 +89,26 @@ public class SubscriptionRegistry {
 		if (!subscriptionExists(address, port)) {
 			return false;
 		}
-		
-		HashMap<Integer, Subscriber> map = activeSubscriptions.get(address);
+		Map<Integer, Subscriber> map = activeSubscriptions.get(address);
+
 		Subscriber s = map.remove(port);
 		s.stopReception();
-		
+
 		// Case 2: more subscriptions for address exist -> do nothing more
 		if (!map.isEmpty()) {
 			return true;
 		}
-		
-		// Case 3: no more subscriptions for address exist -> remove key address
+
+		// Case 3: no more subscriptions for address exist -> remove key
+		// address
 		activeSubscriptions.remove(address);
 		return true;
+
 	}
 
 	/**
-	 * Subscriptions to the given address exist, a value for the given key is present.
+	 * Subscriptions to the given address exist, a value for the given key is
+	 * present.
 	 * 
 	 * @param address
 	 * @return true, if subscriptions exists
@@ -97,7 +118,8 @@ public class SubscriptionRegistry {
 	}
 
 	/**
-	 * A subscription to the given address port combination exists, a value for this combination is present.
+	 * A subscription to the given address port combination exists, a value for
+	 * this combination is present.
 	 * 
 	 * @param address
 	 * @param port
@@ -114,47 +136,52 @@ public class SubscriptionRegistry {
 	}
 
 	/**
-	 * Returns the number of active subscriptions for a given address (over all ports).
+	 * Returns the number of active subscriptions for a given address (over all
+	 * ports).
+	 * 
 	 * @param address
 	 * @return the number or 0
 	 */
-	 public synchronized int getNumberOfActiveSubscriptions(String address) {
-		 if (subscribedToAddress(address)) {
-			 return activeSubscriptions.get(address).size();
-		 }
-		 return 0;
-	 }
-	 
-	 /**
-	  * Returns the number of all active subscriptions for all addresses and ports.
-	  * @return the number or 0
-	  */
-	 public synchronized int getNumberOfActiveSubscriptions() {
-		 if (activeSubscriptions.isEmpty()) {
-			 return 0;
-		 }
-		 int number = 0;
-		 for (String address: activeSubscriptions.keySet()) {
-			 number += getNumberOfActiveSubscriptions(address);
-		 }
-		 return number;
-	 }
-	
-	 /**
-	  * Returns the Subscriber at a given address + port.
-	  * @param address
-	  * @param port
-	  * @return the subscriber or null, if not exists
-	  */
-	 public synchronized Subscriber getSubscriber(String address, int port) {
-		 if (subscriptionExists(address, port)) {
-			 return activeSubscriptions.get(address).get(port);
-		 }
-		 return null;
-	 }
-	 
-	 public synchronized void deleteAllData() {
-		 activeSubscriptions = new HashMap<String, HashMap<Integer, Subscriber>>();
-	 }
+	public synchronized int getNumberOfActiveSubscriptions(String address) {
+		if (subscribedToAddress(address)) {
+			return activeSubscriptions.get(address).size();
+		}
+		return 0;
+	}
+
+	/**
+	 * Returns the number of all active subscriptions for all addresses and
+	 * ports.
+	 * 
+	 * @return the number or 0
+	 */
+	public synchronized int getNumberOfActiveSubscriptions() {
+		if (activeSubscriptions.isEmpty()) {
+			return 0;
+		}
+		int number = 0;
+		for (String address : activeSubscriptions.keySet()) {
+			number += getNumberOfActiveSubscriptions(address);
+		}
+		return number;
+	}
+
+	/**
+	 * Returns the Subscriber at a given address + port.
+	 * 
+	 * @param address
+	 * @param port
+	 * @return the subscriber or null, if not exists
+	 */
+	public synchronized Subscriber getSubscriber(String address, int port) {
+		if (subscriptionExists(address, port)) {
+			return activeSubscriptions.get(address).get(port);
+		}
+		return null;
+	}
+
+	public synchronized void deleteAllData() {
+		activeSubscriptions.clear();
+	}
 
 }
